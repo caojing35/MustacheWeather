a8df2f37d840a5f2f523a5a9422ed7b5ba6257c4
曹晶 <cao.jing.35@hotmail.com>
2017-10-06 21:54:57 +0800

    增加城市数据bean
    增加城市数据bean

---
+++ app/src/main/java/com/mustacheweather/android/db/City.java
@@ -0,0 +1,51 @@
+package com.mustacheweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by caojing on 2017/10/6.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+}
